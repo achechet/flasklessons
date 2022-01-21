142de061bff2a3ba0e0fd3cf1b17eb86808eb03b
achechet <achechet@gmail.com>
2022-01-21 18:30:35 +0300

    none

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# flask content update
+ Работа по совершенствованию сайта pkiCenter
+Update 15.01
---
+++ flsite.py
@@ -0,0 +1,95 @@
+import os
+import sqlite3
+from dotenv import load_dotenv
+from flask import Flask, render_template, url_for, flash, request, session, redirect, abort, g
+from flask.helpers import flash
+from werkzeug.utils import redirect
+from FDataBase import FDataBase
+
+load_dotenv()
+APP_KEY =  os.getenv('APP_KEY')
+PORT = os.getenv('PORT')
+HOST = os.getenv('HOST')
+DATABASE = '/tmp/flsite.db'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = APP_KEY
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db    
+
+@app.route("/")
+def index():
+    db = get_db()
+    dbase = FDataBase(db)
+    print(dbase.getMenu())
+    return render_template('index.html', title="Главная страница", menu = dbase.getMenu())
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title="Страница О нас", menu=menu)
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST' :
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
+
+    return render_template('contact.html', title="Contact", menu=menu)    
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == "alex" and request.form['psw'] =="1234":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+        
+    return render_template('login.html', title="Авторизация", menu=menu)
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('page404.html', title="Страница не найдена", menu=menu), 404 
+
+
+@app.route("/add_post", methods=["POST", "GET"])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+    
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка добавдения статьи', category='error')
+            else:
+                flash('Статья успешно добавлена', category='success')
+        else:
+            flash('Ошибка добавдения статьи', category='error')
+
+    return render_template('add_post.html', title="Добавление статьи", menu = dbase.getMenu() )
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+
+    return f"Профиль пользователя: {username}"
+
+if __name__ == "__main__":
+    app.run(host=HOST, port=PORT, debug=True)
