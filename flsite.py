a23c930489e2ed7b510080d29574bd7768debf8f
achechet <achechet@gmail.com>
2022-02-07 19:18:46 +0300

    Lesson - 16_2

--- flsite.py
+++ flsite.py
@@ -6,7 +6,7 @@ from flask.helpers import flash
 from werkzeug.utils import redirect
 from FDataBase import FDataBase
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, login_user, login_required
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from UserLogin import UserLogin
 
 load_dotenv()
@@ -19,6 +19,9 @@ app.config['SECRET_KEY'] = APP_KEY
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
 
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = "Авторизуйтесь для доступа к закрытым страницам"
+login_manager.login_message_category = "success"
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -75,12 +78,16 @@ def contact():
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('profile'))
+    
     if request.method == 'POST' :
         user = dbase.getUserByEmail(request.form['email'])
         if user and check_password_hash(user['psw'], request.form['psw']):
             userlogin = UserLogin().create(user)
-            login_user(userlogin)
-            return redirect(url_for('index'))
+            rm = True if request.form.get('remainme') else False
+            login_user(userlogin, remember=rm)
+            return redirect(request.args.get("next") or url_for("profile"))
         
         flash("Неверная пара логин/пароль", "error")
         
@@ -136,6 +143,19 @@ def showPost(alias):
         abort(404)
 
     return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("Вы вышли из аккаунта", "success")
+    return redirect(url_for('login'))
+
+@app.route("/post/profile")
+@login_required
+def profile():
+    return f"""<p><a href="{url_for('logout')}"> Выйти из профиля </a>
+                <p> user info: {current_user.get_id()} """
 
 # before_first_request & after_request 
 
